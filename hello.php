7a000a6369c237a9620d71752ba8bc16b8d4aa09
unaiurkiola <unaiurkiola@gmail.com>
2014-10-01 09:33:06 +0200

    version 2.0

--- hello.php
+++ hello.php
@@ -1,7 +1,8 @@
 <html>
 <body>
 
-    Hello <?php echo $_GET["name"]; ?>
-
+    Hello : <?php echo $_GET["name"]; ?><br>
+    Age : <?php echo $_GET["age"]; ?><br>
+    Music :  <?php echo $_GET["music"]; ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,6 +6,13 @@
 
     <form action="hello.php" method="get">
         Name: <input type="text" name="name"><br>
+        Age: <input type="text" name="age"><br>
+        Favorite music: <br>
+        Hard Music<input type="radio" name="music" value="hard"><br>
+		Relaxing Music<input type="radio" name="music" value="relax"> <br>
+
+
+
         <input type="submit">
     </form>
 
